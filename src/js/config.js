e0f6b8f3d38dedbcd94fa4ccd9dcd69e20be05c9
kstee <kstee_90@hotmail.com>
2021-03-10 10:42:59 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7421141d-dc4b-4340-b20c-66838a931adb';
+export const MODAL_CLOSE_SEC = 2.5;
